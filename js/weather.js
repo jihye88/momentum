63a6a15498005186dce385455dcae02bbb5e8dc4
jihye88 <flowerpigkr@gmail.com>
2021-11-08 15:29:19 +0900

    todo, weather
    delete todo list
    weather api

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = '1b68fb9260814bb5fabc772dcb76aa4d';
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
